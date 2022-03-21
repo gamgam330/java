c881aaf75bd062ac57281709e8402c0bc66a2339
gyumin <gamgam0330@naver.com>
2022-03-21 09:06:22 +0900

    [JAVA] 파이 구하기 추가

--- Lec2Ex5.java
+++ Lec2Ex5.java
@@ -4,12 +4,14 @@ public class Lec2Ex5 {
 	public static void main (String args[]) {
 		int curcle;
 		double sum=0;
+		int j = -1;
 		Scanner input = new Scanner(System.in);
 		System.out.printf("반복횟수를 입력하시오 : ");
 		curcle = input.nextInt();
 		
 		for (int i = 1; i < curcle+1; i++) {
-				sum += (Math.pow(-1, i+1))*(4.0/(2*i-1));
+				j *= -1;
+				sum += j*(4.0/(2*i-1));
 		}
 		System.out.println("파이는"+sum+"입니다.");
 	}
