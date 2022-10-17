1f705b6ead865720390f165aea37f492114c56c4
90000e <48207536+gamgam330@users.noreply.github.com>
2022-10-17 22:21:25 +0900

    [JAVA] BOJ
    1427, 11650

---
+++ BOJ11650.java
@@ -0,0 +1,31 @@
+package น้มุ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ11650 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int N = Integer.parseInt(br.readLine());
+		int[][] arr = new int[N][2];
+		
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr, (e1, e2) -> {
+			if(e1[0] == e2[0]) return e2[1] - e1[1];
+			else return e1[0] - e2[0];
+		});
+		
+		for(int i = 0; i < N; i++) {
+			System.out.println(arr[i][0]+" "+arr[i][1]);
+		}
+	}
+}
