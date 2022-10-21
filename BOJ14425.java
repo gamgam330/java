d81cc6086be5b2366c828a615dfd962162dbed6e
90000e <48207536+gamgam330@users.noreply.github.com>
2022-10-21 22:17:01 +0900

    [JAVA] BOJ
    14425

---
+++ BOJ14425.java
@@ -0,0 +1,31 @@
+package น้มุ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class BOJ14425 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		Map<String, Integer> map = new HashMap<>();
+		
+		int cnt = 0;
+		for(int i = 0; i < N; i++) {
+			map.put(br.readLine(), 0);
+		}
+		
+		
+		for(int i = 0; i < M; i++) {
+			if(map.containsKey(br.readLine())) {
+				cnt++;
+			}
+		}
+		System.out.print(cnt);
+	}
+}
