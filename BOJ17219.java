f05031a8cae4b6ca8491121635cbdd8616aaf433
90000e <48207536+gamgam330@users.noreply.github.com>
2022-10-22 19:06:51 +0900

    [JAVA] BOJ
    17219

---
+++ BOJ17219.java
@@ -0,0 +1,32 @@
+package น้มุ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class BOJ17219 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		Map<String, String> map = new HashMap<>(); 
+		
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			map.put(st.nextToken(), st.nextToken());
+		}
+		
+		for(int i = 0; i < M; i++) {
+			String str = br.readLine();
+			if(map.containsKey(str)) {
+				sb.append(map.get(str)).append("\n");
+			}
+		}
+		System.out.print(sb);
+	}
+}
