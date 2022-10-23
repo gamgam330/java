1c12042367cf19878de8a8f70137cfabd273322f
90000e <48207536+gamgam330@users.noreply.github.com>
2022-10-23 21:08:04 +0900

    [JAVA] BOJ
    2167

---
+++ BOJ2167.java
@@ -0,0 +1,41 @@
+package น้มุ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ2167 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		StringBuilder sb = new StringBuilder();
+		int[][] arr = new int[N][M];
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < M; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		int K = Integer.parseInt(br.readLine());
+		int result = 0;
+		for(int z = 0; z < K; z++) {
+			st = new StringTokenizer(br.readLine());
+			int i = Integer.parseInt(st.nextToken()) - 1;
+			int j = Integer.parseInt(st.nextToken()) - 1;
+			int x = Integer.parseInt(st.nextToken()) - 1;
+			int y = Integer.parseInt(st.nextToken()) - 1;
+			result = 0;
+			for(int p = i; p <= x; p++) {
+				for(int w = j; w <= y; w++) {
+					result += arr[p][w];
+				}
+			}
+			sb.append(result).append("\n");
+		}
+		System.out.print(sb);
+	}
+}
