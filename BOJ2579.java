11f4844bab34c9e3613485f17c78519d82a1e45b
90000e <48207536+gamgam330@users.noreply.github.com>
2022-10-27 19:15:50 +0900

    [JAVA] BOJ
    2579

---
+++ BOJ2579.java
@@ -0,0 +1,11 @@
+package น้มุ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ2579 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	}
+}
