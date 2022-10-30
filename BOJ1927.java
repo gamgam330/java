2b34fb8c0c1ab573f3ca6a62bedbc5533c9cca95
90000e <48207536+gamgam330@users.noreply.github.com>
2022-10-30 22:03:27 +0900

    [JAVA] BOJ
    1927

---
+++ BOJ1927.java
@@ -0,0 +1,25 @@
+package น้มุ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class BOJ1927 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		 PriorityQueue<Integer> queue = new PriorityQueue<>(Comparator.comparingInt(x -> x));
+		 
+		 for(int i = 0; i < N; i++) {
+			 int Num = Integer.parseInt(br.readLine());
+			 if(Num == 0) {
+				 if(queue.isEmpty()) System.out.println("0");
+				 else System.out.println(queue.poll());
+			 }
+			 else queue.add(Num);
+		 }
+	}
+}
