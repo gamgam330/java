50c4a33e6a961451b9c8bc346cafb0333e67fbcc
90000e <48207536+gamgam330@users.noreply.github.com>
2022-12-11 16:57:10 +0900

    [JAVA] BOJ
    11659

---
+++ BOJ11659.java
@@ -0,0 +1,31 @@
+package น้มุ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ11659 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[] arr = new int[N+1];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 1; i <= N; i++) {
+			arr[i] = arr[i-1] + Integer.parseInt(st.nextToken());
+		}
+		for(int x = 0; x < M; x++) {
+			int result = 0;
+			st = new StringTokenizer(br.readLine());
+			int i = Integer.parseInt(st.nextToken());
+			int j = Integer.parseInt(st.nextToken());
+			
+			sb.append(arr[j] - arr[i-1]).append("\n");
+		}
+		
+		System.out.print(sb);
+	}
+}
