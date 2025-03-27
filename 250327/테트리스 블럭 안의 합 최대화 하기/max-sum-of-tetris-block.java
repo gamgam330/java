00537a2640821e37db65375975207ed607c75d65
90000e <48207536+gamgam330@users.noreply.github.com>
2025-03-27 15:48:30 +0900

    Title: 테트리스 블럭 안의 합 최대화 하기, Time: 105ms, Memory: 10MB, Status: Passed - Codetree

--- 250327/테트리스 블럭 안의 합 최대화 하기/max-sum-of-tetris-block.java
+++ 250327/테트리스 블럭 안의 합 최대화 하기/max-sum-of-tetris-block.java
@@ -7,6 +7,7 @@ public class Main {
     static int[] dx = {0, 0, -1, 1};
     static int[] dy = {-1, 1, 0, 0};
     static int result;
+    static boolean[][] visited;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -14,7 +15,7 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         map = new int[N][M];
-        int[][] visited = new int[N][M];
+        visited = new boolean[N][M];
         result = 0;
 
         for(int i = 0; i < N; i++){
@@ -26,9 +27,9 @@ public class Main {
 
         for(int i = 0; i < N; i++){
             for(int j = 0; j < M; j++){
-                visited[i][j] = 1;
-                backTracking(i, j, 1, visited);
-                visited[i][j] = 0;
+                visited[i][j] = true;
+                backTracking(i, j, map[i][j], 1);
+                visited[i][j] = false;
                 checkExceptionShape(i, j);
             }
         }
@@ -36,9 +37,9 @@ public class Main {
         System.out.println(result);
     }
 
-    private static void backTracking(int x, int y, int count, int[][] visited){
+    private static void backTracking(int x, int y, int sum, int count){
         if(count == 4){
-            findVisitedBox(visited);
+            result = Math.max(result, sum);
             return;
         }
 
@@ -46,27 +47,12 @@ public class Main {
             int nx = x + dx[i];
             int ny = y + dy[i];
 
-            if(0 <= nx && nx < N && 0 <= ny && ny < M){
-                if(visited[nx][ny] == 0){
-                    visited[nx][ny] = 1;
-                    backTracking(nx, ny, count + 1, visited);
-                    visited[nx][ny] = 0;
-                }
+            if(0 <= nx && nx < N && 0 <= ny && ny < M&& !visited[nx][ny]){
+                visited[nx][ny] = true;
+                backTracking(nx, ny, sum + map[nx][ny], count + 1);
+                visited[nx][ny] = false;
             }
         }
-    }
-
-    private static void findVisitedBox(int[][] visited){
-        int tmp = 0;
-        for(int i = 0 ; i < N; i++){
-            for(int j = 0; j < M; j++){
-                if(visited[i][j] == 1){
-                    tmp += map[i][j];
-                }
-            }
-        }
-
-        result = Math.max(result, tmp);
     }
 
     private static void checkExceptionShape(int x, int y) {
