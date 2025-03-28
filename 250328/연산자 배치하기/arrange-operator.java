000fe06bec3ae0750025b5c92133c68c8b9ae61c
90000e <48207536+gamgam330@users.noreply.github.com>
2025-03-28 13:13:39 +0900

    Title: 연산자 배치하기, Time: 89ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250328/연산자 배치하기/arrange-operator.java
@@ -0,0 +1,63 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int[] arr;
+    static int[] command;
+    static int N;
+    static int max, min;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        
+        arr = new int[N+1];
+        command = new int[3];
+        
+        max = 0;
+        min = Integer.MAX_VALUE - 1;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        
+        for(int i = 0 ; i < N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < 3; i++){
+            command[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        backTracking(arr[0], 1);
+
+        System.out.println(min + " " + max);
+    }
+
+    private static void backTracking(int result, int cnt){
+        if(cnt == N){
+            max = Math.max(max, result);
+            min = Math.min(min, result);
+            return;
+        }
+
+        for(int i = 0; i < 3; i++){
+            if(command[i] > 0){
+                command[i]--;
+                switch (i){
+                    case 0:
+                        backTracking(result + arr[cnt], cnt + 1);
+                        break;
+                    case 1:
+                        backTracking(result - arr[cnt], cnt + 1);
+                        break;
+                    case 2:
+                        backTracking(result * arr[cnt], cnt + 1);
+                        break;
+                }
+                command[i]++;
+            }
+        }
+
+    }
+}
