0e1ead35343a188d9c50359609eba47f97420a08
90000e <48207536+gamgam330@users.noreply.github.com>
2025-03-28 13:17:21 +0900

    Title: 연산자 배치하기, Time: 84ms, Memory: 9MB, Status: Passed - Codetree

--- 250328/연산자 배치하기/arrange-operator.java
+++ 250328/연산자 배치하기/arrange-operator.java
@@ -1,63 +1,58 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[] arr;
-    static int[] command;
     static int N;
-    static long max, min;
+    static int[] arr;
+    static int[] cal;
+    static int max, min;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        
-        arr = new int[N+1];
-        command = new int[3];
-        
-        max = 0;
-        min = Integer.MAX_VALUE - 1;
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        
-        for(int i = 0 ; i < N; i++){
+        arr = new int[N+1];
+        cal = new int[3];
+        max = Integer.MIN_VALUE;
+        min = Integer.MAX_VALUE;
+
+        for(int i = 0; i < N; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
         st = new StringTokenizer(br.readLine());
-
         for(int i = 0; i < 3; i++){
-            command[i] = Integer.parseInt(st.nextToken());
+            cal[i] = Integer.parseInt(st.nextToken());
         }
-        
-        backTracking(arr[0], 1);
 
+        dfs(arr[0], 1);
         System.out.println(min + " " + max);
     }
 
-    private static void backTracking(int result, int cnt){
+    static void dfs(int num, int cnt){
         if(cnt == N){
-            max = Math.max(max, result);
-            min = Math.min(min, result);
+            max = Math.max(max, num);
+            min = Math.min(min, num);
             return;
         }
 
         for(int i = 0; i < 3; i++){
-            if(command[i] > 0){
-                command[i]--;
-                switch (i){
+            if(cal[i] > 0){
+                cal[i]--;
+                switch (i) {
                     case 0:
-                        backTracking(result + arr[cnt], cnt + 1);
-                        break;
+                    dfs(num + arr[cnt], cnt + 1);
+                    break;
                     case 1:
-                        backTracking(result - arr[cnt], cnt + 1);
-                        break;
+                    dfs(num - arr[cnt], cnt + 1);
+                    break;
                     case 2:
-                        backTracking(result * arr[cnt], cnt + 1);
-                        break;
+                    dfs(num * arr[cnt], cnt + 1);
+                    break;
                 }
-                command[i]++;
+                cal[i]++;
             }
         }
-
     }
 }
