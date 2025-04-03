4a66ec0522e636479f96e26cce1c50d27adce46f
90000e <48207536+gamgam330@users.noreply.github.com>
2025-04-03 17:36:27 +0900

    Title: 마법의 숲 탐색, Time: 162ms, Memory: 17MB, Status: Passed - Codetree

---
+++ 250403/마법의 숲 탐색/magical-forest-exploration.java
@@ -0,0 +1,131 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int rSize, cSize;
+    static int k;
+    static int[][] forest;
+    static int[] dy = {-1 ,0, 1, 0};
+    static int[] dx = {0, 1, 0, -1};
+    static int[][] draw = {{-1,0}, {0,1}, {1,0}, {0,-1}};   //상, 우, 하, 좌
+
+    static int x, y, ndir;
+    static boolean[][] visited;
+
+    //아래로, 왼쪽아래, 오른쪽아래 체크해야할 것들
+    static int[][] southCheck = {{1,-1},{2,0},{1,1}};
+    static int[][] westCheck = {{-1,-1},{0,-2},{1,-1},{1,-2},{2,-1}};
+    static int[][] eastCheck = {{0,2},{-1,1},{1,1},{1,2},{2,1}};
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        rSize = Integer.parseInt(st.nextToken());
+        cSize = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        forest = new int[rSize+3][cSize  ];
+        
+        solution(br);
+    }
+
+    private static void solution(BufferedReader br) throws IOException{
+        int sum = 0;
+        StringTokenizer st;
+
+        for (int i = 0; i < k; i++) {
+            st = new StringTokenizer(br.readLine());
+            //x, y는 중앙값
+            x = 1;
+            y = Integer.parseInt(st.nextToken()) - 1;
+            ndir = Integer.parseInt(st.nextToken());
+
+            if(moveGolem()){
+                visited = new boolean[rSize+3][cSize];
+                int bottom = dfs(x, y) - 2;
+
+                sum += bottom;
+            }else{
+                clearForest();
+            }
+        }
+
+        System.out.println(sum);
+    }
+
+
+    private static int dfs(int x, int y) {
+        visited[x][y] = true;
+        int max = x;
+        if(forest[x][y] == 1){
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if(check(nx, ny) && forest[nx][ny] == 3 && !visited[nx][ny]){
+                    max = Math.max(max, dfs(nx, ny));
+                }
+            }
+        }else{
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if(check(nx, ny) && forest[nx][ny] != 0 && !visited[nx][ny]){
+                    max = Math.max(max, dfs(nx, ny));
+                }
+            }
+        }
+        return max;
+    }
+
+    private static boolean moveGolem(){
+        while(true){
+            if(canMove(southCheck)){
+            } else if (canMove(westCheck)) {
+                y -= 1;
+                ndir = (ndir + 3) % 4;
+            } else if (canMove(eastCheck)) {
+                y += 1;
+                ndir = (ndir + 1) % 4;
+            } else{
+                break;
+            }
+            x += 1;
+        }
+        if(x <= 3) return false;
+    
+        for(int i = 0; i < 4; i++){
+            forest[x + draw[i][0]][y + draw[i][1]] = (i == ndir) ? 2 : 1;
+        }
+
+        forest[x][y] = 3;
+
+        return true;
+    }
+
+    private static boolean canMove(int[][] position) {
+        for (int[] next : position) {
+            int nx = x + next[0];
+            int ny = y + next[1];
+
+            if(!check(nx,ny) || forest[nx][ny] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static boolean check(int x, int y){
+        return 0 <= x && x < rSize+3 && 0 <= y && y < cSize;
+    }
+
+    private static void clearForest() {
+        for (int i = 0; i < rSize + 3; i++) {
+            for (int j = 0; j < cSize; j++) {
+                forest[i][j] = 0;
+            }
+        }
+    }
+}
